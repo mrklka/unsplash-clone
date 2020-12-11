415c679f1485a25e72dedffa718ecb27fedfc7c1
mrklka <mrklka99@gmail.com>
2020-12-11 17:29:54 -0600

    Initial Commit: Navbar setup

---
+++ src/components/universal/MainNavbar.js
@@ -0,0 +1,64 @@
+import React from 'react'
+
+
+class MainNavbar extends React.Component {
+
+    render() {
+
+        return (
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+                    <a className="navbar-brand" href="#  ">
+                        <img src="/site_elements/logo.png" width="30" height="30" className="d-inline-block align-top" alt="" />
+                    </a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="# navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <form className="form-inline my-2 my-lg-0">
+                            <button className="btn  my-2 my-sm-0" type="submit">
+                                <img src="/site_elements/nav_search_icon.png" width="18" height="18" alt="" />
+
+                            </button>
+                            <input className="mainnavbar_search form-control mr-sm-2" type="search" placeholder="Search free high-resolution photos" aria-label="Search"></input>
+                        </form>
+
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="# ">Home <span className="sr-only">(current)</span></a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="# ">Brands</a>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="# " id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    ...</a>
+                                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a className="dropdown-item" href="# ">Blog</a>
+                                    <a className="dropdown-item" href="# ">Topics</a>
+                                    <a className="dropdown-item" href="# ">Collections</a>
+                                </div>
+                            </li>
+                            <li className="nav-item">
+                                <button type="button" class="navbar-submit-modal btn btn-outline-dark">Submit A Photo</button>
+                            </li>
+                            <li className="nav-item">
+                                <a className="navbar-brand" href="#  ">
+                                    <img src="/site_elements/nav_avatar.jpg" width="30" height="30" className="d-inline-block align-top" alt="" />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+
+
+
+
+            </div>
+        )
+    }
+}
+
+export default MainNavbar
+
--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.render(
   <React.StrictMode>
     <App />
